499dc3702a474c7ecccbbace35e380d86d4426f5
kavitshah8 <kavit.shah8@gmail.com>
2014-12-02 11:19:30 -0500

    changing scope of this with CALL

---
+++ JS/call.js
@@ -0,0 +1,10 @@
+//  Without modifying this function in any way, how can you execute it in such a way where it will output “Joe is hungry”
+
+function speak(){
+  var reply = [this.person,'is',this.feeling].join(' ');
+  console.log(reply);
+}
+
+var i = {person: 'Joe', feeling: 'hungry'};
+
+speak.call(i);
