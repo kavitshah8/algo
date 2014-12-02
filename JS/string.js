8b77890d12e8aeb615addab90f45a14bddaf2d2b
kavitshah8 <kavit.shah8@gmail.com>
2014-12-02 11:51:56 -0500

    adds a prototype method on string

---
+++ JS/string.js
@@ -0,0 +1,7 @@
+// Make console.log('i am all lowercase'.capitalize()); to print 'I am all lowercase'
+
+String.prototype.capitalize = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+console.log('i am all lowercase'.capitalize());
