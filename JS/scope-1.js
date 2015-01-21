e70d395c89737f2c9b254b41df14cf0a627ecb2f
kavitshah8 <kavit.shah8@gmail.com>
2015-01-21 13:17:03 -0500

    adds strict mode example

--- JS/scope-1.js
+++ JS/scope-1.js
@@ -1,15 +1,16 @@
 (function(){
 
-	var a = 5;
-	b = 5;
+	var a = 5; // a is defined only in this function scope
+
+	b = 5; // b becomes global
 
 })();
 
-if ( typeof a !== 'undefined' ) {
+if ( typeof a !== 'undefined' ) { // Way to check the variable is defined or not
 	
 	console.log(a);
 
 }else{
 
-	console.log(b);
+	console.log(b); // 5
 }
---
+++ JS/use-strict.js
@@ -0,0 +1,9 @@
+(function () {
+
+	'use strict';
+
+	var a  = 5;
+
+	b = 6; // ReferenceError: b is node defined
+
+})();
