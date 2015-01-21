38f4e69fcc87c6072b9d84d9003f5910c5120f6d
kavitshah8 <kavit.shah8@gmail.com>
2015-01-21 13:49:47 -0500

    reanming with minor editing

--- JS/call.js
+++ JS/call.js
@@ -1,7 +1,9 @@
 	//  Without modifying this function in any way, how can you execute it in such a way where it will output “Joe is hungry”
 
-function speak(){
+function speak() {
+
   var reply = [this.person,'is',this.feeling].join(' ');
+
   console.log(reply);
 }
 
--- JS/native-methods-1.js
+++ JS/native-methods-1.js
@@ -1,7 +1,9 @@
 // Make console.log('i am all lowercase'.capitalize()); to print 'I am all lowercase'
 
 String.prototype.capitalize = function() {
+
 	return this.charAt(0).toUpperCase() + this.slice(1);
+
 };
 
 console.log('i am all lowercase'.capitalize());
