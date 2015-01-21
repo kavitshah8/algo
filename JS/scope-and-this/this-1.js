a9dd13c9cb32506434383da64f81848bafc25a30
kavitshah8 <kavit.shah8@gmail.com>
2015-01-21 17:25:19 -0500

    example of this

---
+++ JS/scope-and-this/this-1.js
@@ -0,0 +1,24 @@
+var name = 'Kavit';
+
+var obj = {
+
+	name: 'Teju',
+
+	prop: {
+
+		name: 'Yugam',
+
+		getName: function () {
+
+			return this.name;
+		}
+	}
+
+};
+
+// In JavaScript context of this depends on how a function is invoked, not how it’s defined.
+
+console.log(obj.prop.getName()); // Yugam
+
+var test = obj.prop.getName;
+console.log(test()); // Kavit in browser, but undefined in node environment
