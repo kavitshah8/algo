99bf14c542209afea0015ecf0004ce67ad347608
kavitshah8 <kavit.shah8@gmail.com>
2015-01-22 11:21:36 -0500

    memoized version of fibonacci number series

--- JS/dynamic-programming/fibonacci-numbers.js
+++ JS/dynamic-programming/fibonacci-numbers.js
@@ -1,14 +1,29 @@
-function fibonacci (num) {
+var dictionary = { };
+
+function fibonacci ( num ) {	
+
+	if ( dictionary[num] ) {
+
+		// console.log(dictionary[num]);
+
+		return dictionary[num];
 	
-	if ( typeof num ===  'number' && ( num === 1 || num === 2) ) {
+	}
+
+	if ( typeof num === 'number' && ( num === 1 || num === 2 ) ) {
 
 		return 1;
 	
 	} else {
 
-		return fibonacci (num - 1) + fibonacci (num - 2);
+		var value = fibonacci (num - 1) + fibonacci (num - 2);
 
+		dictionary[num] = value;
+
+		return value;
 	}
+
 }
 
 console.log( fibonacci ( 10 ) );	
+// console.log( dictionary );	
