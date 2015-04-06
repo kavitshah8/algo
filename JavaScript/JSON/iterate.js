c099402f024fc810fbb4551318c983e1c6052953
kavitshah8 <kavit.shah8@gmail.com>
2015-04-06 17:11:31 -0400

    iterating over json

---
+++ JavaScript/JSON/iterate.js
@@ -0,0 +1,12 @@
+var json = {"firstName":"Kavit",
+						"lastName":"Shah",
+						"contactNumber":"408410----"
+					};
+
+for (var key in json){
+
+	if(json.hasOwnProperty(key)){
+
+		console.log(json[key]);
+	}
+}
