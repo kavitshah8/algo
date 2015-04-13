42ea83168a24fb4e77bd20698018cb08676e88c3
kavitshah8 <kavit.shah8@gmail.com>
2015-04-13 16:04:36 -0400

    simple fizzBuzz solution

---
+++ JavaScript/johnsonFit/fizzBuzz.js
@@ -0,0 +1,24 @@
+function fizzBuzz() {
+
+	var i, output;
+
+	for ( i = 1; i < 101; i++) {
+	  
+	  output = '';
+	  
+	  if ( !(i % 3) ) {
+
+	  	output += 'Fizz';
+	  } 
+
+	  if (!(i % 5)) {
+	  	
+	  	output += 'Buzz';
+	  }
+
+	  console.log(output || i);
+	}
+}
+
+fizzBuzz();
+ 
