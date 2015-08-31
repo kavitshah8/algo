befce23d88d45979bed3dad0616b9766362ace2b
Kavit Shah <kavit.shah8@gmail.com>
2015-08-30 17:55:00 -0700

    improves the logic and adds the row, column number for debugging

--- JavaScript/ctci/moderate/tic-tac-toe-winner.js
+++ JavaScript/ctci/moderate/tic-tac-toe-winner.js
@@ -1,43 +1,46 @@
+// tic:'X', tac:'Y', toe: 'Z'
+
 var board = ['Y','Y','Y',
-      		'Y','Y','X',
-      		'X','Y','Y'];
+      		'Y','Y','Z',
+      		'Z','Z','X'];
+
+// var board = ['Y','Y','Y',
+//       		'Y','Y','Z',
+//       		'Y','X','X'];
 
 function findWinner (board) {
-
-  var winner;
-  var row = Math.sqrt(board.length);
   
+  var row, column; 
+  
+  row = 0;
+  column = 0;
+
   //Check rows
-  for (var i = 0; i < board.length; i+= 3) {
+  for (var i = 0; i < board.length; i += 3) {
     
-    if ( (board[i] == 'X') && (board[i+1] == 'X') && (board[i+2] == 'X')) {
-      return 'X';
-    } else if ( (board[i] == 'Y') && (board[i+1] == 'Y') && (board[i+2] == 'Y')) {
-      return 'Y';
+    row++;
+    if ( ( board[i] === board[i+1] ) && ( board[i+1] === board[i+2] ) ) {
+      return board[i] + ' at row : ' + row;
     }
   }
   
   //Check columns
-  for (var i = 0; i < Math.sqrt(board.length); i++) {
+  for (var j = 0; j < Math.sqrt(board.length); j++) {
     
-    if ( (board[i] == 'X') && (board[i+3] == 'X') && (board[i+6] == 'X')) {
-      return 'X';
-    } else if ( (board[i] == 'Y') && (board[i+3] == 'Y') && (board[i+6] == 'Y')) {
-      return 'Y';
+    column++;
+    if ( ( board[j] === board[j+3] ) && ( board[j+3] === board[j+6] ) ) {
+      return board[i] + ' at column : ' + column;
     }
   }
   
   //Check diagonals
-    for (var i = 0; i < (Math.sqrt(board.length) - 1); i++) {
+  for (var k = 0; k < ( Math.sqrt(board.length) - 1 ); k++) {
     
-    if ( (board[i] == 'X') && (board[i+4] == 'X') && (board[i+8] == 'X')) {
-      return 'X';
-    } else if ( (board[i] == 'Y') && (board[i+4] == 'Y') && (board[i+8] == 'Y')) {
-      return 'Y';
+    if ( ( board[k] === board[k+4] ) && ( board[k+4] === board[k+8] ) ) {
+      return board[k];
     }
   }
   
-  return 'D';
 }
 
 console.log('Winnwe is : ' + findWinner(board));
