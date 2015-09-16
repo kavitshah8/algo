7c82981282dc512992aee01536098ff36e671b32
Kavit Shah <kavit.shah8+github@gmail.com>
2015-09-15 19:32:33 -0700

    Update matrix.js

--- JavaScript/johnsonFit/matrix.js
+++ JavaScript/johnsonFit/matrix.js
@@ -1,3 +1,17 @@
+function defaultMatrix1(size) { // Incorrect one
+	// fix me!
+	var defaultValue = 0;
+	var row = [];
+	var matrix = [];
+	for (var i=0; i < size; i++) { row.push(defaultValue); }
+
+	// Issue in here is that the values are being referenced, they are not being copied.
+	// Hence, setting matrx[1][1] = 1 will update matrix[0][1] = matrix[2][1] = matrix[3][1] = matrix[4][1] = 1
+	
+	for (var i=0; i < size; i++) { matrix.push(row); }
+	return matrix;
+}
+
 function defaultMatrix(size) { // returns array
 	// fix me!
 	var x = new Array(size);
