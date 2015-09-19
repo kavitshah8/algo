8b1ea7d7cde197c9f7cf053b0c0d154a2e6de20c
Kavit Shah <kavit.shah8+github@gmail.com>
2015-09-18 18:41:16 -0700

    Update is-odd-integer.js

--- JavaScript/tre-interview/is-odd-integer.js
+++ JavaScript/tre-interview/is-odd-integer.js
@@ -1,23 +1,18 @@
-function isOddInteger ( num ) {
-		
-	if ( typeof( num ) === 'number') {	
-	
-		if ( num % 2 === 1 ) {
-
+function isOdd(num) {
+	if (typeof(num) === 'number') {	
+		if (num % 2 === 1) {
 			return true;
 		}
 		else {
-		
 			return false;
 		}
 	}
 	else {
-
 		return 'Not a number';
 	}
 }
 
-console.log(isOddInteger(2));
-console.log(isOddInteger(2.1));
-console.log(isOddInteger('3'));
-console.log(isOddInteger(3));
+console.log(isOdd(2));  // false
+console.log(isOdd(2.1)); // false
+console.log(isOdd('3')); // Not a number
+console.log(isOdd(3)); // true
