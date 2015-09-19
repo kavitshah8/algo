f93a7808d86144966c80b736088d49556135a39e
Kavit Shah <kavit.shah8+github@gmail.com>
2015-09-19 11:12:50 -0700

    Update is-odd-integer.js

--- JavaScript/tre-interview/is-odd-integer.js
+++ JavaScript/tre-interview/is-odd-integer.js
@@ -1,33 +1,31 @@
 function isOdd(num) {
 	if (typeof num === 'number') {	
-		if (num % 2 === 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return ((num % 2) === 1) || ((num % 2) === -1) ? true : false;
 	}
 	else {
-		return 'Not a number';
+		throw 'Not a number';
 	}
 }
 
-console.log(isOdd(2));  // false
-console.log(isOdd(2.1)); // false
-console.log(isOdd('3')); // Not a number
 console.log(isOdd(3)); // true
+console.log(isOdd(2));  // false
+console.log(isOdd(-3));  // true
+console.log(isOdd(2.1)); // false
+console.log(isOdd('3')); // Uncaught Not a number
 
-
-Number.prototype.isOdd = function(num) {
-	if (typeof num === 'number') {	
-		if (num % 2 === 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	else {
-		return 'Not a number';
-	}	
+Number.prototype.isOdd = function() {
+		return ((this % 2) === 1) || ((this % 2) === -1) ? true : false;
 }
+
+
+var a = -3;
+console.log(a.isOdd());  // true
+
+a = 2.1;
+console.log(a.isOdd()); // false
+
+console.log((3).isOdd()); // true
+console.log((2).isOdd());  // false
+
+a = '3';
+console.log(a.isOdd()); // Uncaught Not a number
