37254ff9fb5a270bdda4ce92af4bb41e9e2e690f
Kavit Shah <kavit.shah8+github@gmail.com>
2015-09-26 23:15:15 -0700

    Update prime-factorization.js

--- JavaScript/algorithms/prime-factorization.js
+++ JavaScript/algorithms/prime-factorization.js
@@ -1,18 +1,18 @@
 function primeFactors(num) {
 	
-	while( num % 2 === 0 ) {
+	while (num % 2 === 0) {
 		console.log(2);
 		num = num / 2;
 	}
 
-	for( var i = 3; i <= Math.sqrt(num); i++) {
-		while( num % i === 0 ){
+	for (var i = 3; i <= Math.sqrt(num); i++) {
+		while (num % i === 0) {
 			console.log(i);
 			num = num / i;
 		}
 	}
 
-	if( num > 2) {
+	if (num > 2) {
 		console.log(num);
 	}
 }
